d4e605fddc2b063ddb6adf2d4aa6cf483f8d330c
alisha_exsete <alisha.banerjee@exsete.com>
2023-04-21 15:37:55 +0530

    First Commit

---
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -0,0 +1,25 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import {Todo} from 'src/app/Todo';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent {
+@Output() todoAdd: EventEmitter<Todo> = new EventEmitter();
+
+title:string = '';
+desc:string = '';
+
+onSubmit(){
+ const todo = {
+  sno: 0,
+  title:this.title, 
+  description: this.desc, 
+  active: true
+ }
+ this.todoAdd.emit(todo);
+}
+
+}
